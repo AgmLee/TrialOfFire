e1c0f633f79fa97d185b2712edde44349d19523d
Kyp01Gaming <agm.l@hotmail.com>
2016-09-23 15:23:12 +1000

    Update

---
+++ Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform playerTransform;
+    public Vector3 direction = Vector3.forward;
+    public float offsetXZ, offsetY, offsetA;
+
+    private Transform myTransform;
+    private Vector3 newDir;
+
+    void Start()
+    {
+        if (playerTransform == null)
+        {
+            GameObject obj = GameObject.FindGameObjectWithTag("Player");
+            if (obj != null)
+            {
+                playerTransform = obj.GetComponent<Transform>();
+            }
+        }
+
+        if (myTransform == null)
+        {
+            myTransform = gameObject.transform;
+        }
+
+        if (myTransform == null || playerTransform == null)
+        {
+            Debug.Log("Missing Transforms");
+            Destroy(this);
+        }
+
+        newDir = direction;
+    }
+
+    void Update()
+    {
+        Vector3 pos = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
+        Vector3 offset = pos + (direction * offsetXZ) + (offsetY * Vector3.up);
+        myTransform.position = Vector3.Lerp(myTransform.position, offset, 5)/* Time.deltaTime*/;
+        myTransform.forward = Vector3.Lerp(direction, newDir, 5);
+    }
+
+    public void SetCameraDir(Vector3 newDirection)
+    {
+        newDir = newDirection;
+    }
+}
---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager inst;
+
+    void Awake()
+    {
+        if (inst == null)
+        {
+            inst = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (inst != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
