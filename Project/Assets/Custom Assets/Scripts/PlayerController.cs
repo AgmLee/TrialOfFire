96a51cf174b67d8c9517bbfb344a24492f6d9847
Lachlan McClymont <lachlanmcclymont25@gmail.com>
2016-10-13 14:24:24 +1100

    Added rigidbody character controller.

--- Project/Assets/Custom Assets/Scripts/PlayerController.cs
+++ Project/Assets/Custom Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
 	Transform thisTransform = null;
 	public int maxSpriteCount;
 	int spriteCount;
-	Vector3 targetVel = Vector3.zero;
+	Vector3 targetDir = Vector3.zero;
 
 	void Start ()
 	{
@@ -26,28 +26,53 @@ public class PlayerController : MonoBehaviour
 	{
 		if (controller.isGrounded) 
 		{
-			//targetVel = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-	
-			if (Input.GetAxis ("Vertical") > 0) 
-			{
-				float camYRot = cameraTransform.localEulerAngles.y;
-				Vector3 targetRot = thisTransform.localEulerAngles;
-				targetRot.y = camYRot;
-				thisTransform.localEulerAngles = targetRot;
-			}
-	
-			targetVel = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-			targetVel = this.transform.TransformDirection (targetVel);
-			targetVel *= movementSpeed;
+			Vector3 forwardDir = cameraTransform.forward * Input.GetAxis ("Vertical");
+			Vector3 rightDir = cameraTransform.right * Input.GetAxis ("Horizontal");
+
+			transform.forward = (forwardDir + rightDir).normalized;
+		
 		
 			if (Input.GetAxis ("Jump") > 0) 
 			{
-				targetVel.y = jumpHeight;
+				targetDir.y = jumpHeight;
 			}
 		}
 		
-		targetVel.y -= gravity * Time.deltaTime;
-		controller.Move (targetVel * Time.deltaTime);
+		targetDir.y -= gravity * Time.deltaTime;
+		controller.Move (Vector3.forward * Time.deltaTime);
+
 	}
+
+	//void Update ()
+	//{
+	//	if (controller.isGrounded) 
+	//	{
+	//		//targetVel = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+	//
+	//		if (Input.GetAxis ("Vertical") > 0) 
+	//		{
+	//			float camYRot = cameraTransform.localEulerAngles.y;
+	//			Vector3 targetRot = thisTransform.localEulerAngles;
+	//			targetRot.y = camYRot;
+	//			thisTransform.localEulerAngles = targetRot;
+	//		}
+	//
+	//		targetVel = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+	//		if (targetVel != Vector3.zero && targetVel != lastAngle) {
+	//			transform.Rotate (Vector3.up, Vector3.Angle (transform.forward, targetVel));
+	//		}
+	//		targetVel = this.transform.TransformDirection (targetVel);
+	//		targetVel *= movementSpeed;
+	//
+	//
+	//		if (Input.GetAxis ("Jump") > 0) 
+	//		{
+	//			targetVel.y = jumpHeight;
+	//		}
+	//	}
+	//	
+	//	targetVel.y -= gravity * Time.deltaTime;
+	//	controller.Move (targetVel * Time.deltaTime);
+	//}
 		
 }
---
+++ Project/Assets/Test Assets/Test Materials/RBCharacterController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RBCharacterController : MonoBehaviour {
+
+	Rigidbody rb = null;
+	public Transform cameraTransform = null;
+	public float movementSpeed = 10;
+	public float rotateSpeed = 2;
+
+	bool horizontalInput;
+	bool verticalInput;
+
+	Vector3 forward;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update ()
+	{
+		horizontalInput = Input.GetAxis ("Horizontal") != 0 ? true : false;
+		verticalInput = Input.GetAxis ("Vertical") != 0 ? true : false;
+
+		if (verticalInput) 
+		{
+			Vector3 rot = transform.localEulerAngles;
+			rot.y = cameraTransform.localEulerAngles.y;
+			transform.localEulerAngles = rot;
+			forward = rot;
+		}
+
+		if (horizontalInput) 
+		{
+			Vector3 rot = transform.localEulerAngles;
+			rot.y = forward.y + (90 * Input.GetAxis ("Horizontal"));
+			transform.localEulerAngles = rot;
+		}
+
+		rb.velocity = transform.forward * (movementSpeed * Input.GetAxis ("Vertical"));
+	}
+}
