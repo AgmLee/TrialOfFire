c7c37ee5e5d16323d621e8e7037e459ebf12731a
Kyp01Gaming <agm.l@hotmail.com>
2016-10-27 13:46:02 +1100

    Audio Update
    Updated the audio on prefabs and added audio to door.

--- Project/Assets/Custom Assets/Scripts/Door.cs
+++ Project/Assets/Custom Assets/Scripts/Door.cs
@@ -3,26 +3,38 @@
 public class Door : MonoBehaviour, IAction
 {
     public Animator door;
+    public AudioClip close;
+    public float openSpeed = 3.0f;
     public bool invert = false;
     private bool isActive = false;
     public bool IsActivated
     {
         get { return isActive; }
     }
-
+    private AudioSource aus;
     void Start()
     {
+        aus = GetComponent<AudioSource>();
         if (invert)
         {
             isActive = !isActive;
         }
         door.SetBool("invert", invert);
         door.SetBool("IsActive", isActive);
+        door.speed = openSpeed;
     }
 
     public void Activation()
     {
         isActive = !isActive;
+        if (isActive)
+        {
+            aus.Play();
+        }
+        else
+        {
+            aus.PlayOneShot(close);
+        }
         door.SetBool("IsActive", isActive);
     }
 }
